347ca0c19a8956bf0f69088631ebbbbe71257690
Jodu555 <Jodu505@gmail.com>
2022-01-14 14:58:23 +0100

    don everything with path resolving

--- src/index.js
+++ src/index.js
@@ -10,8 +10,14 @@ const dirs = fs.readdirSync(filesPath).sort();
 dirs.forEach(dir => {
     if (!exclude.includes(dir)) {
         const name = dir.split('-')[1];
-        const files = fs.readdirSync(path.join(filesPath, dir)).map(e => path.join(filesPath, dir, e));
+        const files = fs.readdirSync(path.join(filesPath, dir)).map(e => {
+            return { path: path.join(filesPath, dir, e), name: e };
+        });
         console.log(files);
-        // fs.copyFileSync()
+        files.forEach(file => {
+            const dest = path.join(AFTERSHOKZ_DIRECTORY, name, file.name)
+            console.log(file.path, dest);
+            // fs.copyFileSync()
+        });
     }
 });
