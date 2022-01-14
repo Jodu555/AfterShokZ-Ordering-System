b5db5db2b1f982fdb6d1d0381ade675adcc1431d
Jodu555 <Jodu505@gmail.com>
2022-01-14 11:21:17 +0100

    Implemented the file listing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path')
 
 const AFTERSHOKZ_DIRECTORY = 'E://';
 const filesPath = 'D://Allgemein//Ich//Hobbys//Musik//Xtrainerz';
@@ -9,5 +10,7 @@ const dirs = fs.readdirSync(filesPath).sort();
 dirs.forEach(dir => {
     if (!exclude.includes(dir)) {
         const name = dir.split('-')[1];
+        const files = fs.readdirSync(path.join(filesPath, dir));
+        console.log(name, files);
     }
 });
