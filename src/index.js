6e5c2119b2e79992eb29ddb40c7a74c491a4c6e4
Jodu555 <Jodu505@gmail.com>
2022-01-14 11:19:20 +0100

    Wrote some name striping

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 
 const AFTERSHOKZ_DIRECTORY = 'E://';
-const filesPath = 'D:\Allgemein\Ich\Hobbys\Musik\Xtrainerz';
+const filesPath = 'D://Allgemein//Ich//Hobbys//Musik//Xtrainerz';
+const exclude = ['OLD'];
 
-const dirs = fs.readdirSync(filesPath);
-console.log(dirs);
+const dirs = fs.readdirSync(filesPath).sort();
+
+dirs.forEach(dir => {
+    const name = dir.split('-')[1];
+    console.log(name);
+});
