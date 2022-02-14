1693d45255904fbab8c065b3c56cd2e3d4767063
Jodu555 <Jodu505@gmail.com>
2022-02-14 17:24:54 +0100

    added the commandmanager

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 const path = require('path')
 
+const { CommandManager } = require('@jodu555/commandmanager');
+//                                              Pass here the standard pipe you want to use
+const commandManager = CommandManager.createCommandManager(process.stdin, process.stdout);
+
 const AFTERSHOKZ_DIRECTORY = 'I://Test';
 const filesPath = 'D://Allgemein//Ich//Hobbys//Musik//Xtrainerz';
 const exclude = ['OLD'];
 
 const dirs = fs.readdirSync(filesPath).sort();
 
+console.log('Started to copy files');
 dirs.forEach(dir => {
     if (!exclude.includes(dir)) {
         const name = dir.split('-')[1];
